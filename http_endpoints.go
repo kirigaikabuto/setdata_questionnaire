55e0b22011485d44ff93e1763e9ac86e040ab80e
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-09-13 20:52:35 +0600

    Sdsddsd

--- http_endpoints.go
+++ http_endpoints.go
@@ -322,9 +322,9 @@ func(h *httpEndpoints) MakeSendOrderEmail() func(w http.ResponseWriter, r *http.
 }
 
 func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Add("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	response, err := json.Marshal(payload)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
