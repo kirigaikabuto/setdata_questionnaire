0bafa9bfee320001405bdb155b577daaa7e567e4
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-09-13 21:04:07 +0600

    commits

--- http_endpoints.go
+++ http_endpoints.go
@@ -279,6 +279,13 @@ func (h *httpEndpoints) MakeListOrderEndpoint(paramName string) func(w http.Resp
 
 func(h *httpEndpoints) MakeSendOrderForConsultation() func(w http.ResponseWriter, r *http.Request){
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		header := w.Header()
 		header.Add("Access-Control-Allow-Origin", "*")
 		header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
