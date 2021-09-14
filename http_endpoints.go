49d26ee81c8d0c751872c2f2830cb0cbf1e27c9a
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-09-14 16:47:32 +0600

    commits

--- http_endpoints.go
+++ http_endpoints.go
@@ -327,6 +327,7 @@ func (h *httpEndpoints) MakeSendOrderEmail() func(w http.ResponseWriter, r *http
 
 func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Credentials" ,"true")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	response, err := json.Marshal(payload)
